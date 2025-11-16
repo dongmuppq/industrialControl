0ac15d8d13453e878928b178707592793ee0ddc2
dongmu <945456685@qq.com>
2025-11-16 16:35:46 +0800

    显示成功

--- gui.c
+++ gui.c
@@ -47,6 +47,14 @@ static const lv_font_t *font_normal;
 
 static lv_obj_t *g_kb;
 
+// 背景框样式变量
+static lv_style_t g_style_cont1;
+static lv_style_t g_style_cont2;
+static lv_style_t g_style_cont2_x;
+static lv_style_t g_style_cont2_x_x;
+static lv_style_t g_style_cont2_x_2_x;
+static lv_style_t g_style_cont2_x_3_x;
+
 // 自定义事件代码，给每个节点的 Send 按钮使用
 static uint32_t MY_LV_EVENT_UPDATE_RPC;     // 更新节点信息事件
 static uint32_t MY_LV_EVENT_READ_PERIOD;    // 周期发送事件
@@ -64,7 +72,7 @@ void gui_start(void)
     init_style();
 
     // 创建全局键盘
-    g_kb = lv_keyboard_create(lv_layer_sys);
+    g_kb = lv_keyboard_create(lv_layer_sys());
     lv_obj_set_size(g_kb, LV_PCT(100), LV_PCT(40));
     lv_obj_add_flag(g_kb, LV_OBJ_FLAG_HIDDEN);
 
@@ -77,10 +85,24 @@ void gui_start(void)
     lv_obj_set_flex_flow(cont1, LV_FLEX_FLOW_ROW);
     lv_obj_set_flex_align(cont1, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
+    lv_obj_t *btn = lv_button_create(cont1);
+    lv_obj_t *label = lv_label_create(btn);
+    lv_label_set_text(label, "Add Node");
+    // lv_obj_add_event_cb(btn, add_new_item_event_handler, LV_EVENT_CLICKED, NULL);
     
 }
 
 void init_style(void)
 {
+    // 顶栏容器背景框样式
+    lv_style_init(&g_style_cont1);
+    lv_style_set_pad_all(&g_style_cont1, 12);
+    lv_style_set_bg_opa(&g_style_cont1, LV_OPA_COVER);
+    lv_style_set_bg_color(&g_style_cont1, lv_color_hex(0xcdcdcd));
+    // lv_style_set_radius(&g_style_cont2, 0);
+    // lv_style_set_border_width(&g_style_cont2, 0);
+    lv_style_set_pad_column(&g_style_cont1, 15);
+    // lv_style_set_pad_row(&g_style_cont2, 18);
+
 
 }
